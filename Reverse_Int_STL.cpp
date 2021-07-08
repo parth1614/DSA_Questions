00f4c677d0a8b21e1e8becd99d4b34ab8cd4d588
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-07-08 14:06:01 +0530

    Update Reverse_Int_STL.cpp

--- Reverse_Int_STL.cpp
+++ Reverse_Int_STL.cpp
@@ -12,16 +12,16 @@ int main(){
     if(x<0){
         string neg = to_string(x); //Converting into string
         reverse(neg.begin()+1, neg.end()); //reverse the whole string except the minus sign i.e. leaving the first index
-        stoi(neg); //converting the string back to int
-    cout<<neg;
+        int rev_neg = stoi(neg); //converting the string back to int
+    cout<<rev_neg;
     }
     
     //When the integer is positive
     else{
         string num = to_string(x);
          reverse(num.begin(), num.end());
-         stoi(num);
-    cout<<num;
+         int rev_num = stoi(num);
+    cout<<rev_num;
     }
     
     
