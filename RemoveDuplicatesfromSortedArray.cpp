e78f6ed768908446148dde2314316c91019b79bf
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-07-11 17:53:27 +0530

    Create RemoveDuplicatesfromSortedArray.cpp

---
+++ RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+void merge(vector<int> &list1){
+    
+for(int i=0;i<list1.size();++i){
+    for(int j=i+1;j<list1.size();++j){
+        if(list1[i]==list1[j]){
+            list1.erase(j);
+        }
+    }
+}
+
+for(int x=0;x<list1.size();++x){
+    cout<<list1[x]<<" ";
+}
+    
+}
+
+int main(){
+    
+    vector<int> list1;
+    
+    int l1;
+    cin>>l1;
+    
+    for(int i=0;i<l1;++i){
+        int x;
+        cin>>x;
+        list1.push_back(x);
+    }
+    
+    
+    merge(list1);
+    
+    
+}
