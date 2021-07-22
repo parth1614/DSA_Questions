4218263d6017a54cc9ac32039f5436acc189555b
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-07-22 12:48:26 +0530

    Create HTML_String_Comp.cpp

---
+++ D.E_Shaw/HTML_String_Comp.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int same(vector<string> &str){
+    int vecsize = str.size();
+    int sum = 0;
+    
+    
+}
+
+int main() {
+	vector<string> str;
+	int n;
+	cin>>n;
+	for(int i=0;i<n;++i){
+	    string x;
+	    cin>>x;
+	    str.push_back(x);
+	}
+}
