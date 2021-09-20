a0de71adc16bbfbc53227fbc79b1e141b589bf7d
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-09-21 00:27:11 +0530

    Create SubsetSum_DP_TopDown.cpp

---
+++ Dynamic Programming/SubsetSum_DP_TopDown.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool subsetSum(int n,int sum, vector<int>& arr){
+    vector<vector<bool>> t;
+    for(int i=0;i<n+1;++i){
+        vector<bool> temp;
+        for(int j=0;j<sum+1;++j){
+            if(i==0){
+               temp.push_back(false);
+            }
+            else if(j==0){
+               temp.push_back(true);
+            }
+        }
+        t.push_back(temp);
+         temp.clear();
+    }
+    
+    for(int i=1;i<n+1;++i){
+        for(int j=1;j<sum+1;++j){
+            if(arr[i-1]<=j){
+                t[i][j] = (t[i-1][j-arr[i-1]] || t[i-1][j]);
+            }
+            else if(arr[i-1]>j){
+                t[i][j] = t[i-1][j];
+            }
+        }
+    }
+    return t[n][sum];
+}
+
+int main(){
+    int sum;
+    cin>>sum;
+    int n;
+    cin>>n;
+    vector<int> arr;
+    
+    for(int i=0;i<n;++i){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    if(subsetSum(n,sum,arr)==true){
+        cout<<"Subset found";
+    }
+    else{
+        cout<<"Subset not found";
+    }
+}
