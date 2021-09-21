b031c5a02a8986c7e8cd2d55c0d0b780697412de
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-09-21 20:29:32 +0530

    Update CountOfSubsetSum_DP_TopDown.cpp

--- Dynamic Programming/CountOfSubsetSum_DP_TopDown.cpp
+++ Dynamic Programming/CountOfSubsetSum_DP_TopDown.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int CountSubsetSum(vector<int>& arr, int n, int sum){
     vector<vector<int>> t;
     
-    for(int i=0;i<n+1;++i){
+     for(int i=0;i<n+1;++i){
         vector<int> temp;
         for(int j=0;j<sum+1;++j){
             if(j==0 || (i==0 && j==0)){
@@ -12,6 +12,9 @@ int CountSubsetSum(vector<int>& arr, int n, int sum){
             }
             else if(i==0){
                 temp.push_back(0);
+            }
+            else{
+                temp.push_back(INT_MAX);
             }
         }
         t.push_back(temp);
@@ -21,7 +24,7 @@ int CountSubsetSum(vector<int>& arr, int n, int sum){
     for(int i=1;i<n+1;++i){
         for(int j=1;j<sum+1;++j){
             if(arr[i-1]<=j){
-                t[i][j] = (t[i-1][j-arr[i-1]] + t[i-1][j]);
+                t[i][j] = t[i-1][j-arr[i-1]] + t[i-1][j];
             }
             else if(arr[i-1]>j){
                 t[i][j] = t[i-1][j];
