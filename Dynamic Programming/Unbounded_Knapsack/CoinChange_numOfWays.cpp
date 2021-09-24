7ee5f98524faf82eebce7f370f93e5485bfb448f
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-09-24 18:35:36 +0530

    Create CoinChange_numOfWays.cpp

---
+++ Dynamic Programming/Unbounded_Knapsack/CoinChange_numOfWays.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int CoinChange(vector<int>& coin, int n, int sum){
+ vector<vector<int>> t;
+ 
+ for(int i=0;i<n+1;++i){
+     vector<int> temp;
+     for(int j=0;j<sum+1;++j){
+         if(j==0 || (i==0&&j==0)){
+             temp.push_back(1);
+         }
+         else if(i==0){
+             temp.push_back(0);
+         }
+         else{
+             temp.push_back(INT_MAX);
+         }
+     }
+     t.push_back(temp);
+     temp.clear();
+   }
+   
+   for(int i=1;i<n+1;++i){
+       for(int j=0;j<sum+1;++j){
+           if(coin[i-1]<=j){
+               t[i][j] = t[i][j-coin[i-1]] + t[i-1][j];
+           }
+           else{
+               t[i][j] = t[i-1][j];
+           }
+       }
+   }
+   
+   return t[n][sum];
+}
+
+int main(){
+    int n;
+    cin>>n;
+    
+    int sum;
+    cin>>sum;
+    
+    vector<int> coin;
+    for(int i=0;i<n;++i){
+        int x;
+        cin>>x;
+        coin.push_back(x);
+    }
+    
+    cout<<"Number of ways: "<<CoinChange(coin,n,sum);
+}
