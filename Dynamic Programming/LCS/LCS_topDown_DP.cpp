e8b5618ecc0fd46f4b809d254bf69c4b8f5e8cfa
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-09-27 19:08:28 +0530

    Create LCS_topDown_DP.cpp

---
+++ Dynamic Programming/LCS/LCS_topDown_DP.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int LCS(string x, string y, int n, int m){
+    vector<vector<int>> t;
+    
+    for(int i=0;i<n+1;++i){
+        vector<int> temp;
+        for(int j=0;j<m+1;++j){
+            if(i==0 || j==0){
+                temp.push_back(0);
+            }
+            else{
+                temp.push_back(INT_MAX);
+            }
+        }
+        t.push_back(temp);
+        temp.clear();
+    }
+    
+    for(int i=1;i<n+1;++i){
+        for(int j=1;j<m+1;++j){
+            if(x[i-1]==y[j-1]){
+                t[i][j] = 1 + t[i-1][j-1];
+            }
+            else{
+                t[i][j] = max(t[i-1][j] , t[i][j-1]); 
+            }
+        }
+    }
+    return t[n][m];
+}
+
+int main(){
+    string x;
+    string y;
+    getline(cin,x);
+    getline(cin,y);
+    
+    int n = x.length();
+    int m = y.length();
+    
+    cout<<LCS(x,y,n,m);
+    
+}
