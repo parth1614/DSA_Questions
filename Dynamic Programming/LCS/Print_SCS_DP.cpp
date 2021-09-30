ce746edffaa3507496c71ddfe54d848cee26aa5a
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-09-30 18:33:20 +0530

    Update Print_SCS_DP.cpp

--- Dynamic Programming/LCS/Print_SCS_DP.cpp
+++ Dynamic Programming/LCS/Print_SCS_DP.cpp
@@ -1,8 +1,12 @@
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    string help(string &str1, string &str2){
+        
+        
+     int n = str1.size();
+     int m = str2.size();
 
-string PrintSCS(string x, string y, int n, int m){
-    vector<vector<int>> t;
+     vector<vector<int>> t;
     for(int i=0;i<n+1;++i){
         vector<int> temp;
         for(int j=0;j<m+1;++j){
@@ -19,7 +23,7 @@ string PrintSCS(string x, string y, int n, int m){
     
     for(int i=1;i<n+1;++i){
         for(int j=1;j<m+1;++j){
-            if(x[i-1]==y[j-1]){
+            if(str1[i-1]==str2[j-1]){
                 t[i][j] = 1 + t[i-1][j-1];
             }
             else{
@@ -28,49 +32,48 @@ string PrintSCS(string x, string y, int n, int m){
         }
     }
     
-    t[n][m] = (m+n) - t[n][m];
-    
-    int i=n; int j=m;
-    string s;
-    while(i<0 && j<0){
-        if(x[i-1]==y[j-1]){
-            s.push_back(x[i-1]);
+    int i=n; 
+    int j=m;
+    string s="";
+    while(i>0 && j>0){
+        if(str1[i-1]==str2[j-1]){
+            s.push_back(str1[i-1]);
             i--;
             j--;
         }
         else{
             if(t[i][j-1]>t[i-1][j]){
-                s.push_back(y[j-1]);
                 j--;
             }
-            else if(t[i-1][j]>t[i][j-1]){
-                s.push_back(x[i-1]);
+            else{
                 i--;
             }
         }
     }
     
-    while(i>0){
-        s.push_back(x[i-1]);
-        i--;
-    }
-    while(j>0){
-        s.push_back(y[j-1]);
-        j--;
-    }
-    
     reverse(s.begin(),s.end());
     return s;
-}
-
-int main(){
-    string x;
-    string y;
-    getline(cin,x);
-    getline(cin,y);
+        
+    }
     
-    int n = x.length();
-    int m = y.length();
-    
-    PrintSCS(x,y,n,m);
-}
+    string shortestCommonSupersequence(string str1, string str2) {
+    string ans="";
+    int i1=0;
+    int i2=0;
+    for(char x:help(str1,str2)){
+        while(str1[i1]!=x){
+            ans=ans+str1[i1];
+            i1++;
+        }
+        while(str2[i2]!=x){
+            ans=ans+str2[i2];
+            i2++;
+        }
+        ans=ans+x;
+        i1++;
+        i2++;
+    }
+    ans=ans+str1.substr(i1)+str2.substr(i2);    
+    return ans;
+    }
+};
