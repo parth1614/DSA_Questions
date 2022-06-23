08599d47d8edde4a1b4f127e84202cee4bde7760
Parth Arora <parth.arora1614@gmail.com>
2022-06-23 12:47:38 +0530

    TLE using Dynamic Programming

---
+++ Placement_Prep/ARRAYS_LB_DSA_SHEET/Min_number_of_jumps.cpp
@@ -0,0 +1,35 @@
+// This is the Dynamic Programming Solution; Time Complexity => O(N^2); Space Complexity => O(N)
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        // Your code here
+   vector<int> jumps;
+   jumps.push_back(0);
+   int min_jump = INT_MAX;
+   int pos = arr[0];
+   
+   for(int i=1;i<n;++i){
+       jumps.push_back(INT_MAX);
+   }
+   
+   for(int i=1;i<n;++i){
+       for(int j=0;j<i;++j){
+           
+               if(i <= j + arr[j]){
+                   jumps[i] = min(jumps[i], jumps[j] + 1);
+               }
+    
+       }
+   }
+   
+   if(jumps[n-1]<0 || jumps[n-1] == INT_MAX){
+       return -1;
+   }
+   
+        return jumps[n-1];
+    }
+};
+
+// -x-x-x-x-x-x-x-x-x-x-x-x-x-  This Solution passes 137/141 test cases, 138th test case causes TLE -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-
+
+
