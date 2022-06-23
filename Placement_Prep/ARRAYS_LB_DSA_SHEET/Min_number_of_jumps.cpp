ca7fa7e840a942268bf85a3361a8e400017b9494
Parth Arora <parth.arora1614@gmail.com>
2022-06-23 15:15:44 +0530

    Working solution in O(N)

--- Placement_Prep/ARRAYS_LB_DSA_SHEET/Min_number_of_jumps.cpp
+++ Placement_Prep/ARRAYS_LB_DSA_SHEET/Min_number_of_jumps.cpp
@@ -45,3 +45,55 @@ class Solution{
 // -x-x-x-x-x-x-x-x-x-x-x-x-x-  This Solution passes 137/141 test cases, 138th test case causes TLE -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-
 
 
+// x-x-x-x-x-x-x-x-x-x-x-x-x-x-x- OPTIMAL SOLUTION BELOW !!! x-x-x-x-x-x-x-x-x-xx-x-x-x-x-x-x-xx-x-x-x-x
+
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        // Your code here
+     int currEnd = 0;
+     int maxReach = 0;
+     int jumps = 0;
+     
+      //Edge Cases//
+     if(n>1)
+       {
+            if(arr[0] == 0){
+            return -1;
+        }    
+        }
+        
+        if(n==1){
+            return 0;
+        }
+     ////////////
+      
+     for(int i=0;i<n;++i){
+        
+        //currEnd = arr[i];
+       //Maximum length the ladder can reach
+        maxReach = max(maxReach,(i + arr[i]));
+        
+       //If the Ladder length Surpases the end of the array, we have found the minimum jumps; We return 1 + jumps, because the last jump had to be counted in the last if-case
+        if(maxReach >= n-1){
+            return 1 + jumps;
+        }
+        
+       // If 'i' reaches till the max length of the ladder; this means that we haven't found any larger ladder, and thus we can't reach the end of the array or move forward
+        if(i == maxReach){
+            return -1;
+        }
+        
+       //If 'i' reaches the current end of the current ladder; and we have found a larger ladder then we increment the jump and make Current end the Maximum reachable lengthh by the new ladder
+        if(i == currEnd){
+            jumps++;
+            currEnd = maxReach;
+        }
+            
+     }
+     
+    }
+};
+// Time Complexity => O(N); Space Complexity => O(1)
+
+// x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-xx-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-END-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-
