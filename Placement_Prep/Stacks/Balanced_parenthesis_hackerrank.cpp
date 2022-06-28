68fb5c068460b888fe1431fa972594eef63689d1
Parth Arora <parth.arora1614@gmail.com>
2022-06-28 15:27:04 +0530

    Create Balanced_parenthesis_hackerrank.cpp

---
+++ Placement_Prep/Stacks/Balanced_parenthesis_hackerrank.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'isBalanced' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+unordered_map<char, int> symbol = {{'{',-1},{'[',-2},{'(',-3},{'}',1},{']',2},{')',3}};
+string isBalanced(string s) {
+    stack<char> st;
+    
+    for(auto it=s.begin();it!=s.end();++it){
+        if(symbol[*it]<0){
+            st.push(*it);
+        }
+        else{
+            if(st.empty()) return "NO";
+            if(symbol[*it] + symbol[st.top()] != 0){
+                return "NO";
+            }
+            else{
+                st.pop();
+            }
+        }
+    }
+    
+    if(st.empty()){
+        return "YES";
+    }
+    else{
+        return "NO";
+    }
+}
+
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = isBalanced(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
