6b1cd5bdb7404f47fc003fd2127cfb9b53c7398f
Parth Arora <parth.arora1614@gmail.com>
2022-07-01 11:38:25 +0530

    Update Overlapping_Intervals.cpp

--- Placement_Prep/ARRAYS_LB_DSA_SHEET/Overlapping_Intervals.cpp
+++ Placement_Prep/ARRAYS_LB_DSA_SHEET/Overlapping_Intervals.cpp
@@ -2,40 +2,25 @@ class Solution {
 public:
     vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
          // Code here
-        //  for(int i=0;i<intervals.size();++i){
-        //      cout<<intervals[i][i]<<" ";
-        //  }
-        //return intervals;
+        vector<vector<int>> ans;
+         sort(intervals.begin(),intervals.end());
+         int start=intervals[0][0];
+         int end=intervals[0][1];
+         
+      for(int i=1;i<intervals.size();i++){
+             if(start<intervals[i][0]&&end<intervals[i][1]&&end<intervals[i][0]){
+                  ans.push_back({start,end});
+                 start=intervals[i][0];
+                  end=intervals[i][1];
+             }
+             else{
+                start=min(start,min(intervals[i-1][0],intervals[i][0]));
+                end=max(end,max(intervals[i-1][1],intervals[i][1]));
+              }
+         }
+         
+        ans.push_back({start,end});
+         return ans; 
         
-        vector<int> vec;
-        
-        for(int i=0;i<intervals.size();++i){
-            vec.push_back(intervals[i][0]);
-            vec.push_back(intervals[i][1]);
-        }
-        
-        sort(vec.begin(), vec.end());
-        
-        intervals.clear();
-        
-        int sum = 0;
-        int start = 0;
-        
-        for(int it = 0;it<vec.size();++it){
-            sum += it;
-            if(sum==(it(it + 1))/2){
-                intervals[i].push_back(start);
-                intervals[i].push_back(it);
-                start = it;
-            }
-           
-        }
-        // int it = 0;
-        
-        // while(sum != (it(it+1))/2){
-        //     sum += it;
-        //     it++;
-        // }
-            
     }
 };
