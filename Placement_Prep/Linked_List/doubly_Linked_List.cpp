5a56b250e6ec5ff69a6a3877fde2f846ea8ca5e7
Parth Arora <parth.arora1614@gmail.com>
2022-07-02 22:49:28 +0530

    Create doubly_Linked_List.cpp

---
+++ Placement_Prep/Linked_List/doubly_Linked_List.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+  
+  public:
+  int data;
+  Node* prev;
+  Node* next;
+  
+  //constructor
+  Node(int d){
+      this -> data = d;
+      this -> prev = NULL;
+      this -> next = NULL;
+  }
+    
+};
+
+int getLen(Node* &head){
+    int len = 0;
+    Node* temp = head;
+    while(temp != NULL){
+        len++;
+        temp = temp -> next;
+    }
+    return len;
+}
+
+void insertAtHead(Node* &head, int d){
+    Node* temp = new Node(d);
+    
+    temp -> next = head;
+    head -> prev = temp;
+    head = temp;
+}
+
+void insertAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    
+    tail->next = temp;
+    temp -> prev = tail;
+    tail = temp;
+}
+
+void insertAtPosition(Node* &head,Node* &tail, int pos, int d){
+    
+    if(pos==1){
+        insertAtHead(head,d);
+        return;
+    }
+    
+    Node* temp = head;
+    int count = 1;
+    while(count < pos-1){
+        temp = temp -> next;
+        count++;
+    }
+    
+    if(temp -> next == NULL){
+        insertAtTail(tail,d);
+        return;
+    }
+    
+}
+
+void print(Node* &head){
+    Node* temp = head;
+    
+    while(temp != NULL){
+        cout<<temp -> data<< " ";
+        temp = temp -> next;
+    }
+    cout<<"Length of Linked List: "<<getLen(head)<<endl;
+}
+
+int main(){
+    
+    Node* newNode = new Node(10);
+    Node* head = newNode;
+    Node* tail = newNode;
+    
+}
