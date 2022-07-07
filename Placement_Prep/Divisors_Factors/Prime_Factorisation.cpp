a9cfee3cf8d7dc4d294b5b2ddf5d7b02aa48e6db
Parth Arora <parth.arora1614@gmail.com>
2022-07-08 00:26:33 +0530

    Solution with Time Complexity=>O(sqrt(N))

--- Placement_Prep/Divisors_Factors/Prime_Factorisation.cpp
+++ Placement_Prep/Divisors_Factors/Prime_Factorisation.cpp
@@ -19,4 +19,33 @@ int main(){
     }
 }
 // This is a brute Force Approach
-// Time Complexity => O(N) and not O(N^2);
+// Time Complexity => O(N) and not O(N^2)
+
+// x-x-x-xx-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x //
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    vector<int> prime_factors;
+    
+    for(int i=2;i*i<=n;++i){
+        while(n%i==0){
+            prime_factors.push_back(i);
+            n = n/i;
+        }
+    }
+    
+    if(n>1){
+        prime_factors.push_back(n);
+    }
+    
+    for(auto prime : prime_factors){
+        cout<<prime<<" ";
+    }
+}
+
+// Time Complexity => O(sqrt(N)) and not O(N^2);
