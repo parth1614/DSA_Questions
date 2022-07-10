d292b659aa0142e81b0cc46c674b6028e2de992d
Parth Arora <parth.arora1614@gmail.com>
2022-07-10 14:29:49 +0530

    Create Detect&Remove_Loop_LL.cpp

---
+++ Placement_Prep/Linked_List/Detect&Remove_Loop_LL.cpp
@@ -0,0 +1,73 @@
+/*************************************************
+    
+    class Node {
+        public :
+
+        int data;
+        Node *next;
+
+        Node(int data) {
+            this -> data = data;
+            this -> next = NULL;
+        }
+    };
+
+*************************************************/
+Node* detectLoop(Node* head){
+    if(head==NULL){
+        return NULL;
+    }
+    if(head->next==NULL){
+        return NULL;
+    }
+    if(head->next=head){
+        return head;
+    }
+    
+    Node* fast = head;
+    Node* slow = head;
+    
+    while(fast!=NULL && slow!=NULL){
+        fast = fast->next;
+        if(fast!=NULL){
+            fast = fast->next;
+        }
+        slow = slow -> next;
+        if(slow == fast){
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+Node* findFirstNode(Node* head){
+    
+    Node* intersection = detectLoop(head);
+    if(intersection == NULL){
+        return NULL;
+    }
+    Node* slow = head;
+    
+    while(slow != intersection){
+        slow = slow->next;
+        intersection = intersection->next;
+    }
+    
+    return slow;
+}
+
+Node *removeLoop(Node *head)
+{
+    if(head == NULL){
+        return NULL;
+    }
+    Node* startOfLoop = findFirstNode(head);
+    Node* temp = head;
+    
+    while(temp->next != startOfLoop){
+        temp = temp->next;
+    }
+    
+    temp->next = NULL;
+    return head;
+}
