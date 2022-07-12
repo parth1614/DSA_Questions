b16638fc2cf4295316291f2054e17fe4dc721c79
Parth Arora <parth.arora1614@gmail.com>
2022-07-12 20:57:31 +0530

    Added Solution "if not allowed to change the node data"

--- Placement_Prep/Linked_List/Sort_LL_0-1-2.cpp
+++ Placement_Prep/Linked_List/Sort_LL_0-1-2.cpp
@@ -57,3 +57,85 @@ Node* sortList(Node *head)
     
     return head;
 }
+// Time Complexity=> O(N); Space Complexity=> O(1)
+
+// x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-xx-x-x-x-x-x-x-x-xx-x-x-x-x-x-x-x-x-x-x-x-x-x-x-xx-x-x-x-x-x-x-x-x-x-x-x-x-x-x-xx-x-x //\
+
+
+// Second Approach: If You are not allowed to change the data values of the nodes, and can only work with the pointers;;
+
+/********************************
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+********************************/
+
+void insertAtTail(Node* &tail, Node* curr){
+    
+    tail->next = curr;
+    tail = curr;
+    
+}
+
+Node* sortList(Node *head)
+{
+     if(head==NULL){
+         return NULL;
+     }
+    
+    Node* zeroHead = new Node(-1);
+    Node* zeroTail = zeroHead;
+    Node* oneHead = new Node(-1);
+    Node* oneTail = oneHead;
+    Node* twoHead = new Node(-1);
+    Node* twoTail = twoHead;
+    
+    Node* curr = head;
+    
+    // Creating the 3 seperate Linked Lists for 0s, 1s, and 2s
+    while(curr != NULL){
+        if(curr->data == 0){
+            insertAtTail(zeroTail,curr);
+        }
+        else if(curr->data == 1){
+            insertAtTail(oneTail,curr);
+        }
+        else if(curr->data == 2){
+            insertAtTail(twoTail,curr);
+        }
+        curr = curr->next;
+    }
+    
+    //Merging the 3 linked Lists
+    
+    // if the linked list of 1s are present
+    if(oneHead->next != NULL){
+        zeroTail->next = oneHead->next;
+        oneTail->next = twoHead->next;
+    }
+    else{
+        zeroTail->next = twoHead->next;
+    }
+    
+    twoTail->next = NULL;
+    
+    //Setting up the head to the first node, i.e. first node with data 0;
+    head = zeroHead->next;
+    
+    //Deleting the dummy nodes
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+  
+    return head;
+}
+// Time Complexity=> o(N); Space Complexity=> O(1)
